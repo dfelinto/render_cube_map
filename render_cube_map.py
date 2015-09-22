d59a8700a74b0da85f54641681661560b607e080
Dalai Felinto <dfelinto@gmail.com>
2015-09-22 18:08:40 -0300

    More work

--- render_cube_map.py
+++ render_cube_map.py
@@ -51,33 +51,99 @@ TODO = True
 # Callbacks
 # ############################################################
 
+class View:
+    def __init__(self, name):
+        self._name = name
+        self._scene = None
+        self._node = None
+        self._camera = None
+
+    def setScene(self, scene):
+        scene.name = self._name
+        self._scene = scene
+
+    def setNode(self, node, links, node_output):
+        node.name = self._name
+        node.label = self._name
+        node.scene = self._scene
+        self._node = node
+
+        # connect to output
+        _input = node_output.layer_slots.new(self._name)
+        links.new(node.outputs[0], _input)
+
+    @property
+    def scene(self):
+        return self._scene
+
+
 @persistent
-def cube_map_render_init(context):
+def cube_map_render_init(scene):
     print('cube_map_render_init')
+
+    if not scene.cube_map.use_cube_map:
+        return
+
+    main_scene = scene
+    hashes = [hash(scene) for scene in bpy.data.scenes]
+
+    views = [
+            View('NORTH'),
+            View('SOUTH'),
+            View('WEST'),
+            View('EAST'),
+            View('ZENITH'),
+            View('NADIR'),
+            ]
+
+    # create cameras
     TODO
+
+    for view in views:
+        # create a scene per view
+        bpy.ops.scene.new(type='LINK_OBJECTS')
+        scene = [scene for scene in bpy.data.scenes if hash(scene) not in hashes][0]
+
+        # mark the scene to remove it afterwards
+        scene.cube_map.is_temporary = True
+
+        hashes.append(hash(scene))
+        view.setScene(scene)
+
+    # create a scene from scratch
+    cubemap_scene = bpy.data.scenes.new('Cube Map')
+    cubemap_scene.cube_map.is_temporary = True
+
+    # created the necessary nodetrees there
+    cubemap_scene.use_nodes = True
+    node_tree = cubemap_scene.node_tree
+
+    # remove all nodes
+    for node in node_tree.nodes:
+        node_tree.nodes.remove(node)
+
+    # output node
+    node_output = node_tree.nodes.new('CompositorNodeOutputFile')
+    node_output.inputs.clear()
+
+    for view in views:
+        node = node_tree.nodes.new('CompositorNodeRLayers')
+        view.setNode(node, node_tree.links, node_output)
 
 
 @persistent
-def cube_map_render_pre(context):
-    print('cube_map_render_pre')
-    TODO
-
-
-@persistent
-def cube_map_render_post(context):
-    print('cube_map_render_post')
-    TODO
-
-
-@persistent
-def cube_map_render_cancel(context):
+def cube_map_render_cancel(scene):
     print('cube_map_render_cancel')
-    TODO
+    cube_map_cleanup(scene)
 
 
 @persistent
-def cube_map_render_complete(context):
+def cube_map_render_complete(scene):
     print('cube_map_render_complete')
+    cube_map_cleanup(scene)
+
+
+def cube_map_cleanup(scene):
     TODO
 
 
@@ -86,7 +152,23 @@ def cube_map_render_complete(context):
 # ############################################################
 
 def RENDER_PT_cube_map(self, context):
-    self.layout.prop(context.scene, "use_cube_map")
+    self.layout.prop(context.scene.cube_map, "use_cube_map")
+
+
+# ############################################################
+# Scene Properties
+# ############################################################
+
+class CubeMapInfo(bpy.types.PropertyGroup):
+    use_cube_map = bpy.props.BoolProperty(
+            name="Cube Map",
+            default=False,
+            )
+
+    is_temporary = bpy.props.BoolProperty(
+            name="Temporary",
+            default=False,
+            )
 
 
 # ############################################################
@@ -94,14 +176,15 @@ def RENDER_PT_cube_map(self, context):
 # ############################################################
 
 def register():
-    bpy.types.Scene.use_cube_map = bpy.props.BoolProperty(
-            name="Cube Map",
-            default=False,
+    bpy.utils.register_class(CubeMapInfo)
+
+    bpy.types.Scene.cube_map = bpy.props.PointerProperty(
+            name="cube_map",
+            type=CubeMapInfo,
+            options={'HIDDEN'},
             )
 
     bpy.app.handlers.render_init.append(cube_map_render_init)
-    bpy.app.handlers.render_pre.append(cube_map_render_pre)
-    bpy.app.handlers.render_post.append(cube_map_render_post)
     bpy.app.handlers.render_cancel.append(cube_map_render_cancel)
     bpy.app.handlers.render_complete.append(cube_map_render_complete)
 
@@ -110,11 +193,12 @@ def register():
 
 def unregister():
     bpy.types.RENDER_PT_render.remove(RENDER_PT_cube_map)
-    del bpy.types.Scene.user_cube_map
+
+    del bpy.types.Scene.cube_map
+    bpy.utils.unregister_class(CubeMapInfo)
+    bpy.utils.unregister_class(HashInfo)
 
     bpy.app.handlers.render_init.remove(cube_map_render_init)
-    bpy.app.handlers.render_pre.remove(cube_map_render_pre)
-    bpy.app.handlers.render_post.remove(cube_map_render_post)
     bpy.app.handlers.render_cancel.remove(cube_map_render_cancel)
     bpy.app.handlers.render_complete.remove(cube_map_render_complete)
 
